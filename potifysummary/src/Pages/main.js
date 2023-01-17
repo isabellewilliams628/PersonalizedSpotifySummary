65ad35973e0d6316d5cdb6b3d2ebaeec186021ab
isabellewilliams628 <72640662+isabellewilliams628@users.noreply.github.com>
2023-01-17 15:25:46 -0600

    Added main page scroll snap

---
+++ potifysummary/src/Pages/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './main.css'
+
+function main() {
+    return (
+      <div className="main">
+        <section className="one">
+            <h1>first page</h1>
+        </section>
+        <section className="two">
+            <h1>second page</h1>
+        </section>
+        <section className="three">
+            <h1>third page</h1>
+        </section>
+      </div>
+    );
+  }
+  
+  export default main;
