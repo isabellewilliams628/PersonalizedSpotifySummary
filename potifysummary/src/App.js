dda593c7cc1d21045a50c6d717fbeab49819e6ab
isabellewilliams628 <72640662+isabellewilliams628@users.noreply.github.com>
2023-01-17 17:06:45 -0600

    Home page start

--- potifysummary/src/App.js
+++ potifysummary/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Main from './Pages/main.js'
+import Main from './Pages/main.js';
+import Home from './Pages/home.js';
+import {Link} from "react-router-dom"
+
 function App() {
   return (
     <div className="App">
       <Main/>
+      <Home/>
     </div>
   );
 }
---
+++ potifysummary/src/Pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.css'
+
+function home() {
+    return (
+      <div className="main">
+        
+      </div>
+    );
+  }
+  
+  export default home;
